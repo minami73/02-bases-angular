29baedf68f18e93e454ce7e0aea9d76f973e9c77
minami73 <jj.c.r.1781@gmail.com>
2023-05-25 17:46:30 -0600

    Avance hasta la clase 39

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+//Este es el corazón del componente, el componente principal
+//Recordemos que lo que tenemos en TS es un simple archivo
+//comun y corriente que contiene una clase
+//Con el decorador que transforma la clase en un componente
+
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
 })
+
 export class AppComponent {
     public title: string = 'Hola Mundo';
-    public counter: number = 10;
+
 }
